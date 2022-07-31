4bc2be64cbb56b854d937a673d9b49dd64bfb83c
vilsonarmani <Vilson.Pereira@ao3tech.com>
2022-07-31 13:28:15 -0300

    Adding context class to my application and setting the definitions of my assembly to database settings

--- CpmPedidos.API/Program.cs
+++ CpmPedidos.API/Program.cs
@@ -1,12 +1,26 @@
 using CpmPedidos.API;
+using Microsoft.EntityFrameworkCore;
+using CpmPedidos.Repository;
+//using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 var services = builder.Services;
+
+services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(
+        /// use this connectionstring
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        /// todas as configurações de banco de dados está no assembly que o meu contexto está
+        /// all the database settings are in the assembly that my context is
+        assembly => assembly.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+
+});
+
 
 /// gestão das classes e das interfaces de forma isolada
 /// management of the classes and the interfaces in isolation
---
+++ CpmPedidos.Repository/Common/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Repository;
+
+public class ApplicationDbContext : DbContext
+{
+    protected ApplicationDbContext()
+    {
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        /// similar use services.Addscoped........
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
